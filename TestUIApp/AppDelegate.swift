280248e1df895457a11aafd9fbab38511e4bb43f
yourPersonalAssistant <maslobeats@gmail.com>
2020-08-18 16:33:25 +0000

    Added AppCenterSDK to project.

--- TestUIApp/AppDelegate.swift
+++ TestUIApp/AppDelegate.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+// AppCenterSDKs.
+import AppCenter
+import AppCenterCrashes
+import AppCenterAnalytics
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,6 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        MSAppCenter.start("f5a413cb-8802-472f-85e1-50e0589c7b3b", withServices:[
+          MSAnalytics.self,
+          MSCrashes.self
+        ])
         return true
     }
 
@@ -32,6 +41,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
